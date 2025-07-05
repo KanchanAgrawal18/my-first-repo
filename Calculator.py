d4d9334818df7a2d9c72cce307b7187bfe39f8c8
KanchanAgrawal18 <agrawalkanchan72@gmail.com>
2025-07-06 00:46:00 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+first_number = float(input("Enter first number:\n"))
+operation = input("Enter operation(+, -, *, /, ^(for square))\n")
+second_number = float(input("Enter second number:\n"))
+if operation == "+":
+    print("Result: " + str(first_number + second_number))
+elif operation == "-":
+    print("Result: " + str(first_number - second_number))
+elif operation == "*":
+    print("Result: " + str(first_number * second_number))
+elif operation == "^":
+    print("Result: " + str(first_number ** second_number))
+else:
+    print("Result: " + str(first_number / second_number))
