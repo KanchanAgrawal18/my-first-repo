d313aee8440ef8d7c02fdd664b4c672958d31b78
KanchanAgrawal18 <agrawalkanchan72@gmail.com>
2025-07-06 01:09:51 +0530

    Added basic Calculator porject

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,5 @@
 first_number = float(input("Enter first number:\n"))
-operation = input("Enter operation(+, -, *, /, ^(for square))\n")
+operation = input("Enter operation(+, -, *, /, ^(for power))\n")
 second_number = float(input("Enter second number:\n"))
 if operation == "+":
     print("Result: " + str(first_number + second_number))
